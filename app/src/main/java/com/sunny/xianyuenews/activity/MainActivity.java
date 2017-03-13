698e29aac3c3299f11045fd2b3fd208943deaf08
SUNny <sunlipeng1994@163.com>
2017-03-13 13:38:21 +0800

    添加样式文件。

--- app/src/main/java/com/sunny/xianyuenews/activity/MainActivity.java
+++ app/src/main/java/com/sunny/xianyuenews/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sunny.xianyuenews;
+package com.sunny.xianyuenews.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sunny.xianyuenews.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/sunny/xianyuenews/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.sunny.xianyuenews.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sunny.xianyuenews.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
