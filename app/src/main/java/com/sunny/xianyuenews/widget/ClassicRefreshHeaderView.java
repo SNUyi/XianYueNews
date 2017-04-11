ce13ad1b8577361bf7d73037c4f3882052fcfd3c
SUNny <sunlipeng1994@163.com>
2017-04-11 08:46:54 +0800

    修改下拉刷新的提示语。

--- app/src/main/java/com/sunny/xianyuenews/widget/ClassicRefreshHeaderView.java
+++ app/src/main/java/com/sunny/xianyuenews/widget/ClassicRefreshHeaderView.java
@@ -78,9 +78,9 @@ public class ClassicRefreshHeaderView extends RelativeLayout implements RefreshT
                     rotated = false;
                 }
 
-                tvRefresh.setText("SWIPE TO REFRESH");
+                tvRefresh.setText("下拉刷新");
             } else {
-                tvRefresh.setText("RELEASE TO REFRESH");
+                tvRefresh.setText("释放立即刷新");
                 if (!rotated) {
                     ivArrow.clearAnimation();
                     ivArrow.startAnimation(rotateUp);
@@ -96,7 +96,7 @@ public class ClassicRefreshHeaderView extends RelativeLayout implements RefreshT
         ivArrow.clearAnimation();
         ivArrow.setVisibility(GONE);
         progressBar.setVisibility(VISIBLE);
-        tvRefresh.setText("REFRESHING");
+        tvRefresh.setText("正在刷新...");
     }
 
     @Override
@@ -111,7 +111,7 @@ public class ClassicRefreshHeaderView extends RelativeLayout implements RefreshT
         ivArrow.clearAnimation();
         ivArrow.setVisibility(GONE);
         progressBar.setVisibility(GONE);
-        tvRefresh.setText("COMPLETE");
+        tvRefresh.setText("刷新成功");
     }
 
     @Override
