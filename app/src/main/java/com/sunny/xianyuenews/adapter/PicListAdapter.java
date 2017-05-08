8de35071be71f8a0829fd36ac8ef924b8e7b8d99
SUNny <sunlipeng1994@163.com>
2017-05-08 18:16:41 +0800

    添加图片模块，待完善。

---
+++ app/src/main/java/com/sunny/xianyuenews/adapter/PicListAdapter.java
@@ -0,0 +1,105 @@
+package com.sunny.xianyuenews.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.aspsine.irecyclerview.IViewHolder;
+import com.bumptech.glide.Glide;
+import com.sunny.xianyuenews.R;
+import com.sunny.xianyuenews.bean.PicListBean;
+
+import java.util.ArrayList;
+
+/**
+ * 作者：SUNny on 2017/5/8 0008 16:29
+ * 邮箱：sunlipeng1994@163.com
+ */
+
+public class PicListAdapter extends RecyclerView.Adapter<PicListAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<PicListBean> mPicListBeens;
+    private OnItemClickListener mOnItemClickListener;
+
+    public PicListAdapter(Context context, ArrayList<PicListBean> picListBeens) {
+        mContext = context;
+        mPicListBeens = picListBeens;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(mContext, R.layout.item_pic_layout, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        PicListBean picListBean = mPicListBeens.get(position);
+
+        String imageSrc = picListBean.getCover();
+        String title = picListBean.getSetname();
+
+        holder.tv_title.setText(title);
+
+        Glide.with(mContext)
+                .load(imageSrc)
+                .placeholder(R.drawable.defaultbg)
+                .crossFade()
+                .into(holder.iv_pic);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getIAdapterPosition();
+                final PicListBean picListBean = mPicListBeens.get(position);
+                if (mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(position, picListBean, v);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPicListBeens.size();
+    }
+
+    /**
+     * 设置Item点击监听
+     *
+     * @param listener
+     */
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
+
+
+    public interface OnItemClickListener<T> {
+        void onItemClick(int position, T t, View v);
+    }
+
+    public class ViewHolder extends IViewHolder {
+
+        public LinearLayout rl_root;
+        public ImageView iv_pic;
+        public TextView tv_title;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            rl_root = (LinearLayout) itemView.findViewById(R.id.rl_root);
+            iv_pic = (ImageView) itemView.findViewById(R.id.iv_pic);
+            tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+        }
+    }
+
+
+}
--- app/src/main/java/com/sunny/xianyuenews/fragment/PhotoFragment.java
+++ app/src/main/java/com/sunny/xianyuenews/fragment/PhotoFragment.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.channelmanager.ProjectChannelBean;
 import com.sunny.xianyuenews.R;
+import com.sunny.xianyuenews.Utils.CategoryDataUtils;
 import com.sunny.xianyuenews.adapter.FixedPagerAdapter;
+import com.sunny.xianyuenews.fragment.photo.PicListFragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +25,7 @@ import java.util.List;
  */
 
 public class PhotoFragment extends BaseFragment {
+
     private final String TAG = PhotoFragment.class.getSimpleName();
 
     private TabLayout mTabLayout;
@@ -39,22 +44,66 @@ public class PhotoFragment extends BaseFragment {
     }
 
     @Override
-    public void initView() {
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Toolbar myToolbar = initToolbar(mView, R.id.my_toolbar, R.id.toolbar_title, R.string.picture_home);
+        initView();
+    }
 
+    @Override
+    public void initView() {
+        mTabLayout = (TabLayout) mView.findViewById(R.id.tab_layout);
+        mNewsViewpager = (ViewPager) mView.findViewById(R.id.news_viewpager);
+        mView.findViewById(R.id.change_channel).setVisibility(View.GONE);
+        channelBeanList = CategoryDataUtils.getPicCategoryBeans();
+        initValidata();
+        initListener();
     }
 
     @Override
     public void initValidata() {
+        fixedPagerAdapter = new FixedPagerAdapter(getChildFragmentManager());
 
-    }
+        fragments = new ArrayList<BaseFragment>();
+        for (int i = 0; i < channelBeanList.size(); i++) {
+            // "推荐","","0031"
+            // "明星","","0003"使用瀑布流
+            ProjectChannelBean channelBean = channelBeanList.get(i);
+            //通知图片列表fragment显示的哪个频道的图片
+            BaseFragment fragment = PicListFragment.newInstance(channelBean.getTid(), channelBean.getColumn());
+            fragments.add(fragment);
+        }
 
-    @Override
-    public void initListener() {
+        fixedPagerAdapter.setChannelBean(channelBeanList);
+        fixedPagerAdapter.setFragments(fragments);
 
+        mTabLayout.setupWithViewPager(mNewsViewpager);
+        mNewsViewpager.setAdapter(fixedPagerAdapter);
+        // mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE); //适合很多tab
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED); // tablayout均分，适合少Tablayout
     }
 
     @Override
     public void bindData() {
+    }
 
+    @Override
+    public void initListener() {
+        mNewsViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
